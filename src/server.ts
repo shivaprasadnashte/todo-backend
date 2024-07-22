dfa05417398dea19111cd69a4734ebdb0854578d
shivaprasad-enalo <shivaprasad.n@enalo,in>
2024-07-22 19:42:08 +0530

    Add cors middleware to server.js

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import connectDb from "./utills/db";
 import userRoute from "./routes/auth.routes";
 import todoRoutes from "./routes/todo.routes"
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
 app.use(express.json());
+app.use(cors());
 const PORT =process.env.PORT|| 3000;
 connectDb();  
 app.get("/", (req: Request, res: Response) => {
